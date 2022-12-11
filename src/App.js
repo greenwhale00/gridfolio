d421ca9a4f04503b0292236ba27cb905f26fa490
greenwhale00 <goreakay@gmail.com>
2022-12-11 22:12:48 +0900

    221211

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Wrapper>
+      {/* <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/sub01' element={<Magslide />} />
+        <Route path='/sub02' element={<Store />} />
+      </Routes> */}
+    </Wrapper>
   );
 }
 
