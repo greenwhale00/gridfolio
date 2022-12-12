22da8afe78f1a506b1a68eed050364113cbc7443
greenwhale00 <goreakay@gmail.com>
2022-12-12 16:48:17 +0900

    221212

--- src/Main.js
+++ src/Main.js
@@ -4,77 +4,21 @@ import { Link } from 'react-router-dom';
 const Main = () => {
     return (
         <section className='Main'>
+
             <div className="inner">
-                <div className="whole">
 
-                    <div class="one name">
-                        <Link to="/about">
-                            <img src={process.env.PUBLIC_URL + "/assets/images/one.jpg"} alt="" />
-                            <div className="frdes">
-                                <p>Hey, I'm Hye-lim</p>
-                            </div>
-                        </Link>
-                    </div>
+                <ul className="whole">
 
-                    <div class="two name">
-                        <Link to="/resume">
-                            <img src={process.env.PUBLIC_URL + "/assets/images/two.jpg"} alt="" />
-                            <div className="frdes">
-                                <p>see my resume</p>
-                            </div>
-                        </Link>
-                    </div>
+                    <li className='one name'><Link to="/about"><div className="frdes">Hey,</div></Link></li>
+                    <li className='two name'><Link to="/resume"><div className="frdes">resume</div></Link></li>
+                    <li className='three name'><Link to="/"><div className="frdes">zegna</div></Link></li>
+                    <li className='four name'><Link to="/"><div className="frdes">kinfolk</div></Link></li>
+                    <li className='five name'><Link to="/"><div className="frdes">balumuda</div></Link></li>
+                    <li className='six name'><Link to="/"><div className="frdes">모름</div></Link></li>
+                    <li className='seven name'><Link to="/"><div className="frdes">get in touch</div></Link></li>
+                    <li className='eight name'><Link to="/"><div className="frdes">what i do</div></Link></li>
 
-                    <div class="three name">
-                        <a href="">
-                            <img src={process.env.PUBLIC_URL + "/assets/images/three.jpg"} alt="" />
-                            <div className="frdes">
-                                <p>kinfolk</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="four name">
-                        <a href="">
-                            <img src={process.env.PUBLIC_URL + "/assets/images/four.jpg"} alt="" />
-                            <div className="frdes">
-                                <p>Zegna</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="five name">
-                        <a href="">
-                            <img src={process.env.PUBLIC_URL + "/assets/images/five.jpg"} alt="" />
-                            <div className="frdes">
-                                <p>balmuda</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="six name">
-                        <a href="">
-                            <img src={process.env.PUBLIC_URL + "/assets/images/six.jpg"} alt="" />
-                            <div className="frdes">
-                                <p>하나 더</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="seven name">
-                        <a href="">
-                            <img src={process.env.PUBLIC_URL + "/assets/images/six.jpg"} alt="" />
-                            <div className="frdes">
-                                <p>Get in touch now</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="eight name">
-                        <a href="">
-                            <img src={process.env.PUBLIC_URL + "/assets/images/six.jpg"} alt="" />
-                            <div className="frdes">
-                                <p>what i do</p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+                </ul>
             </div>
         </section >
     )
