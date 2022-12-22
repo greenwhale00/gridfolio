c3f2a08b6607ba1dde225065cf23b72179c3d68f
greenwhale00 <goreakay@gmail.com>
2022-12-23 00:55:18 +0900

    221223

--- src/About.js
+++ src/About.js
@@ -1,12 +1,37 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 
 
 const About = () => {
     return (
         <section className='About'>
             <div className="inner">
-                ddd
+
+                <p className="first">
+                    About Me
+                </p>
+
+                <div className="whole">
+
+                    <Link to="/about" className='one name'>
+                        <img src={process.env.PUBLIC_URL + "/assets/zegna.webp"} alt="" />
+                    </Link>
+
+                    <Link to="/resume" className='two name'>
+                        <img src={process.env.PUBLIC_URL + "/assets/kinfolk.jpg"} alt="" />
+                    </Link>
+
+                    <Link to="/" className='three name'>
+                        <img src={process.env.PUBLIC_URL + "/assets/balmuda.jpg"} alt="" />
+                    </Link>
+
+                    <Link to="/" className='four name'>
+                        <img src={process.env.PUBLIC_URL + "/assets/idontknow.webp"} alt="" />
+                    </Link>
+
+
+
+                </div>
             </div>
         </section>
     )
--- src/Works.js
+++ src/Works.js
@@ -5,12 +5,12 @@ const Works = () => {
     return (
 
         <section className='Works'>
-
+            {/* 
             <div className="inner">
                 <p className="first">
                     All Works
                 </p>
-            </div>
+            </div> */}
 
 
             <div className="inner">
@@ -23,47 +23,22 @@ const Works = () => {
                 <div className="whole">
 
                     <Link to="/about" className='one name'>
-                        <div className="frdes title">
-                            <img src={process.env.PUBLIC_URL + "/assets/face.png"} className="face" alt="" />
-                            <span>Hi.  I’m Hye-lim.</span>
-                        </div>
+                        <img src={process.env.PUBLIC_URL + "/assets/zegna.webp"} alt="" />
                     </Link>
 
                     <Link to="/resume" className='two name'>
-                        <div className="des">
-                            <span>Learn about me</span>
-                            <p>See my resume</p>
-                        </div>
-
+                        <img src={process.env.PUBLIC_URL + "/assets/kinfolk.jpg"} alt="" />
                     </Link>
 
                     <Link to="/" className='three name'>
-                        <img src={process.env.PUBLIC_URL + "/assets/zegna.webp"} alt="" />
-                        <div className="frdes"></div>
+                        <img src={process.env.PUBLIC_URL + "/assets/balmuda.jpg"} alt="" />
                     </Link>
 
                     <Link to="/" className='four name'>
-                        <img src={process.env.PUBLIC_URL + "/assets/kinfolk.jpg"} alt="" />
-                        <div className="frdes"></div>
-                    </Link>
-
-                    <Link to="/" className='five name'>
-                        <img src={process.env.PUBLIC_URL + "/assets/balmuda.jpg"} alt="" />
-                        <div className="frdes"></div>
-                    </Link>
-
-                    <Link to="/" className='six name'>
                         <img src={process.env.PUBLIC_URL + "/assets/idontknow.webp"} alt="" />
-                        <div className="frdes"></div>
                     </Link>
 
-                    <Link to="/" className='seven name'>
-                        <div className="frdes">get in touch</div>
-                    </Link>
 
-                    <Link to="/" className='eight name'>
-                        <div className="frdes">what i do</div>
-                    </Link>
 
                 </div>
 
