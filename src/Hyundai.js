6631ade491ce137f0310ad8e484850aa9bd0a90d
greenwhale00 <goreakay@gmail.com>
2022-12-23 16:58:49 +0900

    221223

---
+++ src/Hyundai.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'Hyndai.scss'
+
+const Hyundai = () => {
+    return (
+        <section className='Hyundai'>
+            <div className="inner">
+
+            </div>
+        </section>
+    )
+}
+
+export default Hyundai
