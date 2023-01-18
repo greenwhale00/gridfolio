6e5dfc0543da4fead94d6eb22cf12e994921bdb5
greenwhale00 <goreakay@gmail.com>
2023-01-18 22:20:07 +0900

    230118

--- src/Resume.js
+++ src/Resume.js
@@ -5,15 +5,18 @@ const Resume = () => {
     return (
         <section className='Resume'>
             <div className="inner">
-                <div className="left">
-                    profile
+                <div className="whole">
+                    <div className="left">
+                        profile
+                    </div>
+                    <div className="right">
+                        <h3>About me</h3>
+                        <p></p>
+                        <h3>Experience</h3>
+                        <h3>Education</h3>
+                    </div>
                 </div>
-                <div className="right">
-                    <h3>About me</h3>
-                    <p></p>
-                    <h3>Experience</h3>
-                    <h3>Education</h3>
-                </div>
+
             </div>
         </section>
     )
