c14fb745a9c7aada199f6dd1cb9de794ed362c7d
greenwhale00 <goreakay@gmail.com>
2023-01-27 22:08:15 +0900

    230127

--- src/Resume.js
+++ src/Resume.js
@@ -7,13 +7,14 @@ const Resume = () => {
             <div className="inner">
                 <div className="whole">
                     <div className="left">
-                        profile
+                        <h2>Kim Hye-lim</h2>
                     </div>
                     <div className="right">
                         <h3>About me</h3>
                         <p>I’m a frontend develper, I have worked in multidisciplinary fields, in both large and small companies, startups and as a freelancer.</p>
                         <h3>Experience</h3>
                         <h3>Education</h3>
+
                     </div>
                 </div>
 
